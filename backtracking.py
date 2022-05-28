04411f9090d92d3d8f89912612bfba840c49e01f
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2022-05-28 16:31:21 +0200

    csp class added
    Co-Authored-By: LinaAyman31 <77002601+LinaAyman31@users.noreply.github.com>

---
+++ backtracking.py
@@ -0,0 +1,66 @@
+class CSP ():
+    def __init__(self, variables, domains, neighbors, constraints):
+
+        variables = variables or list(domains.keys())
+        self.variables = variables
+        self.domains = domains
+        self.neighbors = neighbors
+        self.constraints = constraints
+        self.initial = ()
+        self.curr_domains = None
+        self.nassigns = 0
+
+    def choices(self, var):
+        return (self.curr_domains or self.domains)[var]   
+
+    def conflicts(self, var, val, assignment):
+        # Subclasses may implement this more efficiently
+        def conflict(var2):
+            return (var2 in assignment and
+                    not self.constraints(var, val, var2, assignment[var2]))
+        #return count(conflict(v) for v in self.neighbors[var])         
+        for v in self.neighbors:
+            if(conflict(v)):
+                return True
+        return False  
+
+    def assign(self, var, val, assignment):
+        """Add {var: val} to assignment; Discard the old value if any."""
+        assignment[var] = val
+        self.nassigns += 1          
+
+    
+    def support_pruning(self):
+        """Make sure we can prune values from domains. (We want to pay
+        for this only if we use it.)"""
+        if self.curr_domains is None:
+            self.curr_domains = {v: list(self.domains[v]) for v in self.variables}
+
+    def suppose(self, var, value):
+        """Start accumulating inferences from assuming var=value."""
+        self.support_pruning()
+        removals = [(var, a) for a in self.curr_domains[var] if a != value]
+        self.curr_domains[var] = [value]
+        return removals    
+
+    def restore(self, removals):
+        """Undo a supposition and all inferences from it."""
+        for B, b in removals:
+            self.curr_domains[B].append(b)    
+
+    def unassign(self, var, assignment):
+        if var in assignment:
+            del assignment[var]        
+
+    def prune(self, var, value, removals):
+        """Rule out var=value."""
+        self.curr_domains[var].remove(value)
+        if removals is not None:
+            removals.append((var, value))  
+
+    def goal_test(self, state):
+        """The goal is to assign all variables, with all constraints satisfied."""
+        assignment = dict(state)
+        return (len(assignment) == len(self.variables)
+                and all(self.conflicts(variables, assignment[variables], assignment) == False
+                        for variables in self.variables))  
